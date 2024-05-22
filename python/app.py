d05d69d5e08623875f7a37c33570dfe6475b2a3c
Grazieli <grazielimartins5@gmail.com>
2024-05-22 16:40:05 -0300

    python

---
+++ python/app.py
@@ -0,0 +1,22 @@
+#capitais = {"Brasil": "Brasília", "Alemanha": "Berlim", "Japão": "Tóquio"}
+#pais = "Brasil"
+
+#if pais in capitais:
+#    capital = capitais[pais]
+ #   print(f"A capital de {pais} é {capital}.")
+#else:
+ #   print(f"A capital de {pais} não foi encontrada no dicionário!")
+
+
+#name = input('Whats your name? ')
+#print('Hi ' + name)
+
+#name = input('Whats your name? ')
+#color = input('Whats your favorite color? ')
+#print(name + ' likes ' + color)
+
+#birth_year = input('Birth Year: ')
+#age = 2024 - int(birth_year)
+#print(age)
+
+ 
